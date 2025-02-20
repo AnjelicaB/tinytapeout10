98d49cbcc09500df9f0dc1a508b34394732ef17f
yb265 <yb265@cornell.edu>
2025-02-20 18:05:22 -0500

    More debugging

--- src/project.v
+++ src/project.v
@@ -16,13 +16,8 @@ module tt_um_AnjelicaB_Top (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
   wire [7:0] in = ui_in;
-  wire [7:0] out = uo_out;
+  wire [7:0] out;
 
   // Instantiate the design
   dev_by_five dev_by_five_inst (
